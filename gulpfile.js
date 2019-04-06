c0083ea9175de8f114a4191552968524e9d06e2e
MeedFine <meedfine@gmail.com>
2019-04-06 14:56:17 +0800

    自动刷新

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require("gulp");
+var browserSync = require("browser-sync");
+var reload = browserSync.reload;
+var nodemon = require("gulp-nodemon");
+
+gulp.task("node", function() {
+  nodemon({
+    script: "./bin/www",
+    ext: "js html",
+    env: {
+      NODE_ENV: "development"
+    }
+  });
+});
+
+gulp.task("server", ["node"], function() {
+  var files = ["views/**/*.html", "views/**/*.hbs", "public/**/*.*"];
+
+  browserSync.init(files, {
+    proxy: "http://localhost:4444",
+    // browser: "chrome",
+    notify: false,
+    port: 4445
+  });
+
+  gulp.watch(files).on("change", reload);
+});
